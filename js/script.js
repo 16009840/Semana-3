a47afde299bd28262ec7e78c24fb4968a9914993
16009840 <fernando.bonilla@galileo.edu>
2024-02-13 15:47:16 -0600

    semana#3

---
+++ js/script.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('select');
+    var instances = M.FormSelect.init(elems);
+});
+
+function clearInput() {
+    document.getElementById('texto1').value = '';
+    document.getElementById('texto2').value = '';
+    document.getElementById('texto3').value = '';
+    document.getElementById('texto4').value = '';
+    document.getElementById('texto5').value = '';
+    document.getElementById('texto6').value = '';
+    document.getElementById('miSelect').value = '';
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('select');
+    var instances = M.FormSelect.init(elems);
+});
+
+function clearInput2() {
+    document.getElementById('test5').value = '';
+    document.getElementById('test6').value = '';
+}
